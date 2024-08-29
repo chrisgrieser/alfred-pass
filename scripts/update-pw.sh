e09e159969640e2f3e996f47c56bf436e77f91a1
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-08-29 15:13:02 +0200

    perf: run auto-push in non-blocking manner

--- scripts/update-pw.sh
+++ scripts/update-pw.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env zsh
-# shellcheck disable=2154
 
 entry="$*"
 pass generate --in-place "$entry" > /dev/null
 pass show "$entry" | head -n1
-
-# shellcheck disable=2154
-[[ "$auto_push" == "1" ]] && pass git push &> /dev/null
